ef2d0b9e3884b68ace4e2e953df9c171006fb779
brtmht <121924604+brtmht@users.noreply.github.com>
2023-01-10 13:14:55 +0530

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -4,7 +4,7 @@ const https = require('https')
 
 function readWriteAsync () {
   // Create string of markdown to be inserted
-  const updatedMd = `URL: [brt](http://localhost:3000/) \n \n \n ![alt text](https://github.com/brtmht/-cicd-_snapshot-backend/blob/main/screenshot.png?raw=true)`
+  const updatedMd = `URL: [brt](https://cicd-snapshot-frontend.vercel.app/) \n \n \n ![alt text](https://github.com/brtmht/-cicd-_snapshot-backend/blob/main/screenshot.png?raw=true)`
   // Update README using FS
   fs.readFile('README.md', 'utf-8', (err, data) => {
     if (err) {
